e1b2922875650000952edf9b11ba1f34ee859663
godric <none@none>
2015-07-09 10:16:04 +0300

    Добавление файл для ввод-вывода данных

---
+++ classes/inout.php
@@ -0,0 +1,35 @@
+<?php
+
+
+function readExp(&$exp1, &$exp2) {
+	
+	$file = fopen('..\testinput\expession.txt', "r");
+	if ($file !== FALSE) {
+		$exp1 = fgets($file);
+		$exp2 = fgets($file);
+		fclose($file);
+	}
+	else {
+		throw new Exception("No file input");
+	}
+	
+	if (strlen($exp1) == 0) {
+		throw new Exception("No expession in file");
+	}
+	if (strlen($exp2) == 0) {
+		throw new Exception("Only one expression in file");
+		
+	}
+}
+
+function buildTree($expession, $typeOfVars) {
+	
+}
+
+
+function printTreeToDOT($file, $curNode) {
+	
+}
+
+
+?>
